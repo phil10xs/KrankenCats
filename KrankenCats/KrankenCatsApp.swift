1c8ae01e5ae443d98ed77330181e082c44014302
Phil N <philipnduka92@gmail.com>
2025-01-29 14:24:51 +0000

    Initial Commit

---
+++ KrankenCats/KrankenCatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KrankenCatsApp.swift
+//  KrankenCats
+//
+//  Created by Philip Igboba on 29/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct KrankenCatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
